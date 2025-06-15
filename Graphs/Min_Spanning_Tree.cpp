c35c44f7744fc1a91b1565c2ed3d1e0ae4d30d9d
Gungun Soni <gungunbps2018@gmail.com>
2025-06-15 16:41:51 +0530

    Added Prims Algorithm

---
+++ Graphs/Min_Spanning_Tree.cpp
@@ -0,0 +1,8 @@
+// Spanning Tree
+// It has (v-1) edges and all the vertices are connected.
+// Tree => no cycles
+
+// Minimun Spanning Tree (Min weight Spanning Tree)
+// Two Algo -> 
+// Prims's Algorithm
+// Kruskal's Algorithm
