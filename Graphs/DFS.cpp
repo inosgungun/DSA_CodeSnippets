f8cb0a8026671641d69a1e4ffeb4b35a4186c28f
Gungun Soni <gungunbps2018@gmail.com>
2025-08-01 09:56:30 +0530

    Added basic bfs and dfs traversal

---
+++ Graphs/DFS.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+  private:
+    void dfs(int node, vector<int> adj[], vector<int> &vis, vector<int> &ls){
+        vis[node] = 1;
+        ls.push_back(node);
+        //traverse all its neighbour
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                dfs(it, adj, vis, ls);
+            }
+        }
+    }
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int> vis(V, 0);
+        int start = 0;
+        vector<int> ls;
+        dfs(start, adj, vis, ls);
+        return ls;
+    }
+};
